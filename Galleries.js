29e50b7ce49b981fa5566ccdb8e2b792183873be
Jason <jason@Tentaquil.(none)>
2012-08-07 15:51:30 +1200

    herp

---
+++ Galleries.js
@@ -0,0 +1,21 @@
+var Galleries = [
+	{
+		title: "2011",
+		images: [
+			{ uri: "img4.jpg", title: "House On The Corner; 2011. C-print, 97 x 119.3 cm" },
+			{ uri: "img9.jpg", title: "Deconstruction; 2011. C-print, 144.7 x 187.7 cm" },
+			{ uri: "img6.jpg", title: "Morvern Road; 2011. C-print, 144.7 x 180.3 cm" },
+		],
+	},
+	{
+		title: "2010",	
+		images: [
+			{ uri: "img1.jpg", title: "Spring Afternoon, Dominion Road; 2010. C-print, 149 x 288.5 cm" },
+			{ uri: "img2.jpg", title: "Early Summer, Marsden Avenue; 2010. C-print, 149 x 187.3 cm" },
+			{ uri: "img3.jpg", title: "Passing Cloud, Pine Street; 2010. C-print, 97.3 x 119.4 cm" },
+			{ uri: "img5.jpg", title: "Late Afternoon, Mt Eden; 2010. C-print, 108.3 x 134.6 cm" },
+			{ uri: "img8.jpg", title: "Summer, Hendon Avenue; 2010. C-print, 85.5 x 104.1 cm" },
+			{ uri: "img7.jpg", title: "Matipo Street; 2010. C-print, 132.2 x 240 cm" },
+		],
+	},
+]
