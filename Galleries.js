d87f5fc81f81eb5b1bdcd7cc6e783d92bfbfa387
Jason <jason@master5o1.com>
2012-08-16 15:20:30 +1200

    things and derp.

--- Galleries.js
+++ Galleries.js
@@ -1,23 +1,28 @@
 var Galleries = [
 	{
-		title: "2012 &ndash;",
+		title: "2012",
 		images: [
-			{ file: "world.jpg", title: "Earth Map, Testing; 2010. C-print, 149 x 288.5 cm" },
+			{ file: "world.jpg", title: "Earth Map, Testing; 2012." },
 			{ file: "img10.jpg", title: "Spring Afternoon, Dominion Road; 2010. C-print, 149 x 288.5 cm [1100px by 557px]" },
 		]
 	},
 	{
-		title: "2010 &ndash; 11",	
+		title: "2011",	
+		images: [
+			{ file: "img4.jpg", title: "House On The Corner; 2011. C-print, 97 x 119.3 cm" },
+			{ file: "img9.jpg", title: "Deconstruction; 2011. C-print, 144.7 x 187.7 cm" },
+			{ file: "img6.jpg", title: "Morvern Road; 2011. C-print, 144.7 x 180.3 cm" },
+		]
+	},
+	{
+		title: "2010",
 		images: [
 			{ file: "img1.jpg", title: "Spring Afternoon, Dominion Road; 2010. C-print, 149 x 288.5 cm" },
 			{ file: "img2.jpg", title: "Early Summer, Marsden Avenue; 2010. C-print, 149 x 187.3 cm" },
 			{ file: "img3.jpg", title: "Passing Cloud, Pine Street; 2010. C-print, 97.3 x 119.4 cm" },
-			{ file: "img4.jpg", title: "House On The Corner; 2011. C-print, 97 x 119.3 cm" },
 			{ file: "img5.jpg", title: "Late Afternoon, Mt Eden; 2010. C-print, 108.3 x 134.6 cm" },
-			{ file: "img6.jpg", title: "Morvern Road; 2011. C-print, 144.7 x 180.3 cm" },
-			{ file: "img7.jpg", title: "Matipo Street; 2010. C-print, 132.2 x 240 cm" },
 			{ file: "img8.jpg", title: "Summer, Hendon Avenue; 2010. C-print, 85.5 x 104.1 cm" },
-			{ file: "img9.jpg", title: "Deconstruction; 2011. C-print, 144.7 x 187.7 cm" },
+			{ file: "img7.jpg", title: "Matipo Street; 2010. C-print, 132.2 x 240 cm" },
 		]
-	},
+	}
 ]
